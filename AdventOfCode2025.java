9fba97f94f26970795893b447bcf96bb6b9596af
MrBadAxe <mrbadaxe@gmail.com>
2025-12-02 21:53:30 -0500

    day 02: part 02

--- AdventOfCode2025.java
+++ AdventOfCode2025.java
@@ -15,42 +15,40 @@ public class AdventOfCode2025{
   }
 
   public static void main(String[] args){
-    System.out.println("Day 01 Part 01: " + Day01.getPart01(readInput("./input_day01.txt")));
-    System.out.println("Day 01 Part 02: " + Day01.getPart02(readInput("./input_day01.txt")));
-
-    /*
-    System.out.println("Day 02 Part 01: " + Day02.getPart01(readInput("./input_day02.txt")));
+    //System.out.println("Day 01 Part 01: " + Day01.getPart01(readInput("./input_day01.txt")));
+    //System.out.println("Day 01 Part 02: " + Day01.getPart02(readInput("./input_day01.txt")));
+    
+    //System.out.println("Day 02 Part 01: " + Day02.getPart01(readInput("./input_day02.txt")));
     System.out.println("Day 02 Part 02: " + Day02.getPart02(readInput("./input_day02.txt")));
 
-    System.out.println("Day 03 Part 01: " + Day03.getPart01(readInput("./input_day03.txt")));
-    System.out.println("Day 03 Part 02: " + Day03.getPart02(readInput("./input_day03.txt")));
+    //System.out.println("Day 03 Part 01: " + Day03.getPart01(readInput("./input_day03.txt")));
+    //System.out.println("Day 03 Part 02: " + Day03.getPart02(readInput("./input_day03.txt")));
 
-    System.out.println("Day 04 Part 01: " + Day04.getPart01(readInput("./input_day04.txt")));
-    System.out.println("Day 04 Part 02: " + Day04.getPart02(readInput("./input_day04.txt")));
+    //System.out.println("Day 04 Part 01: " + Day04.getPart01(readInput("./input_day04.txt")));
+    //System.out.println("Day 04 Part 02: " + Day04.getPart02(readInput("./input_day04.txt")));
 
-    System.out.println("Day 05 Part 01: " + Day05.getPart01(readInput("./input_day05.txt")));
-    System.out.println("Day 05 Part 02: " + Day05.getPart02(readInput("./input_day05.txt")));
+    //System.out.println("Day 05 Part 01: " + Day05.getPart01(readInput("./input_day05.txt")));
+    //System.out.println("Day 05 Part 02: " + Day05.getPart02(readInput("./input_day05.txt")));
 
-    System.out.println("Day 06 Part 01: " + Day06.getPart01(readInput("./input_day06.txt")));
-    System.out.println("Day 06 Part 02: " + Day06.getPart02(readInput("./input_day06.txt")));
+    //System.out.println("Day 06 Part 01: " + Day06.getPart01(readInput("./input_day06.txt")));
+    //System.out.println("Day 06 Part 02: " + Day06.getPart02(readInput("./input_day06.txt")));
 
-    System.out.println("Day 07 Part 01: " + Day07.getPart01(readInput("./input_day07.txt")));
-    System.out.println("Day 07 Part 02: " + Day07.getPart02(readInput("./input_day07.txt")));
+    //System.out.println("Day 07 Part 01: " + Day07.getPart01(readInput("./input_day07.txt")));
+    //System.out.println("Day 07 Part 02: " + Day07.getPart02(readInput("./input_day07.txt")));
 
-    System.out.println("Day 08 Part 01: " + Day08.getPart01(readInput("./input_day08.txt")));
-    System.out.println("Day 08 Part 02: " + Day08.getPart02(readInput("./input_day08.txt")));
+    //System.out.println("Day 08 Part 01: " + Day08.getPart01(readInput("./input_day08.txt")));
+    //System.out.println("Day 08 Part 02: " + Day08.getPart02(readInput("./input_day08.txt")));
 
-    System.out.println("Day 09 Part 01: " + Day09.getPart01(readInput("./input_day09.txt")));
-    System.out.println("Day 09 Part 02: " + Day09.getPart02(readInput("./input_day09.txt")));
+    //System.out.println("Day 09 Part 01: " + Day09.getPart01(readInput("./input_day09.txt")));
+    //System.out.println("Day 09 Part 02: " + Day09.getPart02(readInput("./input_day09.txt")));
 
-    System.out.println("Day 10 Part 01: " + Day10.getPart01(readInput("./input_day10.txt")));
-    System.out.println("Day 10 Part 02: " + Day10.getPart02(readInput("./input_day10.txt")));
+    //System.out.println("Day 10 Part 01: " + Day10.getPart01(readInput("./input_day10.txt")));
+    //System.out.println("Day 10 Part 02: " + Day10.getPart02(readInput("./input_day10.txt")));
 
-    System.out.println("Day 11 Part 01: " + Day11.getPart01(readInput("./input_day11.txt")));
-    System.out.println("Day 11 Part 02: " + Day11.getPart02(readInput("./input_day11.txt")));
+    //System.out.println("Day 11 Part 01: " + Day11.getPart01(readInput("./input_day11.txt")));
+    //System.out.println("Day 11 Part 02: " + Day11.getPart02(readInput("./input_day11.txt")));
 
-    System.out.println("Day 12 Part 01: " + Day12.getPart01(readInput("./input_day12.txt")));
-    System.out.println("Day 12 Part 02: " + Day12.getPart02(readInput("./input_day12.txt")));
-    */
+    //System.out.println("Day 12 Part 01: " + Day12.getPart01(readInput("./input_day12.txt")));
+    //System.out.println("Day 12 Part 02: " + Day12.getPart02(readInput("./input_day12.txt")));
   }
 }
--- Day02.java
+++ Day02.java
@@ -10,6 +10,23 @@ public class Day02{
         return a.equals(b);
     }
 
+    public static boolean isMultiRepeatedNumber(long n){
+        String str = Long.toString(n);
+        int len = str.length();
+        for(int k=1;k<len;k++){
+            if(len%k == 0){
+                boolean allMatch = true;
+                String a = str.substring(0,k);
+                for(int j=k;j<len;j+=k){
+                    String b = str.substring(j,j+k);
+                    if(!a.equals(b)){ allMatch = false; }
+                }
+                if(allMatch){return true; }
+            }
+        }
+        return false;
+    }
+
     public static String getPart01(List<String> input){
         String list = input.get(0);
         String[] ranges = list.split(",");
@@ -20,6 +37,22 @@ public class Day02{
             long ub = Long.parseLong(bounds[1]);
             for(long k = lb; k <= ub; k++){
                 if(isRepeatedNumber(k)){
+                    repeatedNumberTotal += k;
+                }
+            }
+        }
+        return Long.toString(repeatedNumberTotal);
+    }
+    public static String getPart02(List<String> input){
+        String list = input.get(0);
+        String[] ranges = list.split(",");
+        long repeatedNumberTotal = 0;
+        for(String range : ranges){
+            String[] bounds = range.split("-");
+            long lb = Long.parseLong(bounds[0]);
+            long ub = Long.parseLong(bounds[1]);
+            for(long k = lb; k <= ub; k++){
+                if(isMultiRepeatedNumber(k)){
                     repeatedNumberTotal += k;
                 }
             }
