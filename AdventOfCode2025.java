0eb0d7ebeb29a1049303d55c78f54874efd62008
MrBadAxe <mrbadaxe@gmail.com>
2025-12-04 21:04:41 -0500

    day 04: parts 01 and 02

--- AdventOfCode2025.java
+++ AdventOfCode2025.java
@@ -21,11 +21,11 @@ public class AdventOfCode2025{
     //System.out.println("Day 02 Part 01: " + Day02.getPart01(readInput("./input_day02.txt")));
     //System.out.println("Day 02 Part 02: " + Day02.getPart02(readInput("./input_day02.txt")));
 
-    System.out.println("Day 03 Part 01: " + Day03.getPart01(readInput("./input_day03.txt")));
-    System.out.println("Day 03 Part 02: " + Day03.getPart02(readInput("./input_day03.txt")));
+    //System.out.println("Day 03 Part 01: " + Day03.getPart01(readInput("./input_day03.txt")));
+    //System.out.println("Day 03 Part 02: " + Day03.getPart02(readInput("./input_day03.txt")));
 
-    //System.out.println("Day 04 Part 01: " + Day04.getPart01(readInput("./input_day04.txt")));
-    //System.out.println("Day 04 Part 02: " + Day04.getPart02(readInput("./input_day04.txt")));
+    System.out.println("Day 04 Part 01: " + Day04.getPart01(readInput("./input_day04.txt")));
+    System.out.println("Day 04 Part 02: " + Day04.getPart02(readInput("./input_day04.txt")));
 
     //System.out.println("Day 05 Part 01: " + Day05.getPart01(readInput("./input_day05.txt")));
     //System.out.println("Day 05 Part 02: " + Day05.getPart02(readInput("./input_day05.txt")));
---
+++ Day04.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+public class Day04{
+    private static PaperRollGrid generatePaperRollGrid(List<String> input){
+        PaperRollGrid grid = new PaperRollGrid(input.size(), input.get(0).length(), '.');
+        for(int row=0;row<grid.getHeight();row++){
+            String line = input.get(row);
+            for(int col=0;col<grid.getWidth();col++){
+                grid.set(row,col,line.charAt(col));
+            }
+        }
+        return grid;
+    }
+    public static String getPart01(List<String> input){
+        PaperRollGrid grid = generatePaperRollGrid(input);
+        System.out.println(grid);
+        return Integer.toString(grid.findMovableRolls().size());
+    }
+    public static String getPart02(List<String> input){
+        int totalMovable = 0;
+        PaperRollGrid grid = generatePaperRollGrid(input);
+        System.out.println(grid);
+        while(grid.findMovableRolls().size() > 0){
+            totalMovable += grid.findMovableRolls().size();
+            grid = grid.moveRolls(grid.findMovableRolls());
+        }
+        return Integer.toString(totalMovable);
+    }
+}
