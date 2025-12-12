64f6f01d549ce0f387f2beca90a427c900616863
MrBadAxe <mrbadaxe@gmail.com>
2025-12-12 12:06:04 -0500

    day 12: Present and Region classes

---
+++ Present.java
@@ -0,0 +1,5 @@
+public class Present extends CharGrid{
+    public Present(int x, int y, char init){
+        super(x,y,init);
+    }
+}
---
+++ Region.java
@@ -0,0 +1,5 @@
+public class Region extends CharGrid{
+    public Region(int x, int y, char init){
+        super(x,y,init);
+    }
+}
