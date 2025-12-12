0316dee938a11824e629b1e6b5c03f9b8e609cf5
MrBadAxe <mrbadaxe@gmail.com>
2025-12-12 12:17:37 -0500

    day 12: actually we can go line by line instead of grabbing everything at once

--- Day12.java
+++ Day12.java
@@ -16,52 +16,30 @@ public class Day12{
         }
         return z;
     }
-    public static ArrayList<Region> generateRegionsList(List<String> input){
-        int spaceCount = input.size()-30;
-        ArrayList<Region> z = new ArrayList<>();
-        for(int spaceIndex = 0; spaceIndex < spaceCount; spaceIndex++){
-            String line = input.get(spaceIndex + 30);
-            String[] split1 = line.split(":");
-            String[] split2 = split1[0].split("x");
-            Region ps = new Region(Integer.parseInt(split2[0]), Integer.parseInt(split2[1]), '.');
-            z.add(ps);
-        }
-        return z;
-    }
-    public static int[][] generatePresentCountList(List<String> input){
-        int spaceCount = input.size()-30;
-        int[][] z = new int[spaceCount][6];
-        for(int spaceIndex = 0; spaceIndex < spaceCount; spaceIndex++){
-            String line = input.get(spaceIndex + 30);
-            String[] split1 = line.split(":");
-            String[] split2 = split1[1].strip().split("\s+");
-            for(int k=0;k<split2.length;k++){
-                z[spaceIndex][k] = Integer.parseInt(split2[k]);
-            }
-        }
-        return z;
-    }
-
 
     public static String getPart01(List<String> input){
         ArrayList<Present> presents = generatePresentsList(input);
         for(Present p : presents){
             System.out.println(p.toString());
         }
+        for(int spaceIndex = 30; spaceIndex < input.size(); spaceIndex++){
+            String line = input.get(spaceIndex);
+            String[] split1 = line.split(":");
 
-        ArrayList<Region> regions = generateRegionsList(input);
-        for(Region r : regions){
-            System.out.println(r.toString());
-        }
-        
-        int[][] presentCounts = generatePresentCountList(input);
-        for(int k=0;k<presentCounts.length;k++){
-            for(int j=0;j<presentCounts[k].length;j++){
-                System.out.print(presentCounts[k][j] + " ");
+            String[] split2 = split1[0].split("x");
+            Region r = new Region(Integer.parseInt(split2[0]), Integer.parseInt(split2[1]), '.');
+            System.out.println(r);
+
+            String[] split3 = split1[1].strip().split("\s+");
+            int[] presentCounts = new int[6];
+            for(int k=0;k<presentCounts.length;k++){
+                presentCounts[k] = Integer.parseInt(split3[k]);
+            }
+            for(int k=0;k<presentCounts.length;k++){
+                System.out.print(presentCounts[k] + " ");
             }
             System.out.print("\n");
         }
-        
 
         return "";
     }
