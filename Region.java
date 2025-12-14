d19aad539b9d795bfdf049901db77f330506e70e
MrBadAxe <mrbadaxe@gmail.com>
2025-12-13 22:59:11 -0500

    day 12: Region class

--- Region.java
+++ Region.java
@@ -2,4 +2,26 @@ public class Region extends CharGrid{
     public Region(int x, int y, char init){
         super(x,y,init);
     }
+
+    public boolean canPlace(Present p, int posRow, int posCol){
+        for(int row=0;row<p.getHeight();row++){
+            for(int col=0;col<p.getWidth();col++){
+                if(!BoundsChecker.checkBounds(new Point(row,col),this) || this.get(posRow+row,posCol+col) != '.'){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public boolean place(Present p, int posRow, int posCol){
+        if(!canPlace(p,posRow,posCol)){ return false; }
+        for(int row=0;row<p.getHeight();row++){
+            for(int col=0;col<p.getWidth();col++){
+                if(p.get(posRow+row,posCol+col) == '#'){
+                    this.set(posRow+row,posCol+col,'#');
+                }
+            }
+        }
+        return true;
+    }
 }
