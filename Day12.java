c5bef09b03844e183c51c89b4bafe8bcebbd4a2f
MrBadAxe <mrbadaxe@gmail.com>
2025-12-13 23:05:30 -0500

    day 12: part 01

--- Day12.java
+++ Day12.java
@@ -19,29 +19,52 @@ public class Day12{
 
     public static String getPart01(List<String> input){
         ArrayList<Present> presents = generatePresentsList(input);
-        for(Present p : presents){
+        /*for(Present p : presents){
             System.out.println(p.toString());
-        }
+        }*/
+
+        int canFitCount = 0;
+        int inconclusive = 0;
         for(int spaceIndex = 30; spaceIndex < input.size(); spaceIndex++){
             String line = input.get(spaceIndex);
             String[] split1 = line.split(":");
 
             String[] split2 = split1[0].split("x");
             Region r = new Region(Integer.parseInt(split2[0]), Integer.parseInt(split2[1]), '.');
-            System.out.println(r);
+            //System.out.println(r);
+            int regionCells = (r.getHeight() * r.getWidth());
+            int regionUnpackedPresents = ((r.getHeight() / 3)*(r.getWidth() / 3));
+            //System.out.println("cells: " + regionCells + ", 3x3 areas: " + regionUnpackedPresents);
 
             String[] split3 = split1[1].strip().split("\s+");
             int[] presentCounts = new int[6];
             for(int k=0;k<presentCounts.length;k++){
                 presentCounts[k] = Integer.parseInt(split3[k]);
             }
+            int totalPresents = 0;
+            int totalPresentCells = 0;
             for(int k=0;k<presentCounts.length;k++){
-                System.out.print(presentCounts[k] + " ");
+                totalPresents += presentCounts[k];
+                totalPresentCells += presents.get(k).sizeInCells() * presentCounts[k];
+                //System.out.print(presentCounts[k] + " ");
             }
-            System.out.print("\n");
-        }
+            //System.out.println(" = " + totalPresents + "(" + totalPresentCells + ")");
 
-        return "";
+            if(totalPresents <= regionUnpackedPresents){
+                canFitCount++;
+            }else if(totalPresentCells < regionCells){
+                //System.out.println("maybe?");
+                inconclusive++;
+                /*
+                 if we had more presents than there were 3x3 areas in the region,
+                 but fewer total present cells than the total region cells,
+                 we might have to do some packing
+                 but we didn't (inconclusive = 0)
+                */
+            }
+        }
+        //System.out.println(inconclusive);
+        return Long.toString(canFitCount);
     }
     public static String getPart02(List<String> input){
         return "";
